909d89f86065581d9b49dd424f533f37ce4b5dbf
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-10-23 18:07:58 -0500

    adding modules for app

---
+++ Develop/theQuestions.js
@@ -0,0 +1,10 @@
+// required for application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+// questions array for readme.md
+
+const questions = [
+    
+
+]
