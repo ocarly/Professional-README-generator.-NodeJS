25c65e9f7898c539637858984fab7eb909b2acce
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-10-23 21:50:18 -0500

    installed correct packages and imported my steez

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const path = require('path');
 // TODO: Create an array of questions for user input
 const questions = [];
 
