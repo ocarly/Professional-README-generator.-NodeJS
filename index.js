8e9828cb154facdb4ce70432e2835bef7c4b346c
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-10-23 22:00:06 -0500

    created inquirer object

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 const path = require('path');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+type: "input", 
+name: "title",
+message: "What is the title of your project?"
+},
+{
+type: "list",
+name: "license",
+message: "what license do ya need?",
+choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+inquirer.prompt(questions).then((answers) => {
+console.log(answers)
+writeToFile("README.MD", generateMarkdown(answers))
+})}
 
 // Function call to initialize app
 init();
